bd5b4486108bdd107a4103b4c9de32aa27ad5d9e
Sylvain K <53495850+skamdem@users.noreply.github.com>
2021-06-12 20:16:45 +0000

    Completed Part II

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,17 +9,9 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [];
-questions[0] = "Who was the first American woman in space?";
-questions[1] = "True or false: 5 kilometer == 5000 meters?";
-questions[2] = "(5 + 3)/2 * 10 = ?";
-questions[3] = "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?";
-questions[4] = "What is the minimum crew size for the ISS?";
+questions.push("Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? ");
 let correctAnswers = [];
-correctAnswers[0] = "Sally Ride";
-correctAnswers[1] = "true";
-correctAnswers[2] = "40";
-correctAnswers[3] = "Trajectory";
-correctAnswers[4] = "3";
+correctAnswers.push("Sally Ride", "true", "40", "Trajectory", "3");
 let candidateAnswers = [];
 
 
@@ -32,17 +24,27 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   const input = require('readline-sync');
-  candidateAnswer = input.question(question);
+  // candidateAnswer = input.question(question);
+  for (let i = 0; i < questions.length; i++){
+    candidateAnswers.push(input.question(questions[i]));
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   let feedback = "Correct answer!";
-  if (candidateAnswer !== correctAnswer){
-    feedback = "Wrong answer";
+  // if (candidateAnswer !== correctAnswer){
+  //   feedback = "Wrong answer";
+  // }
+  // console.log(feedback);
+  for (let i = 0; i < questions.length; i++){
+    feedback = "Correct answer!";
+    if (candidateAnswers[i].toLowerCase !== correctAnswers[i].toLowerCase){
+      feedback = "Wrong answer";
+    }
+    console.log(`${candidateAnswers[i]} ?= ${correctAnswers[i]}, ${feedback}`);
   }
-  console.log(feedback);
   let grade;
   
 
