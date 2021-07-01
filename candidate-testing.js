78af9adc4e2e225a60d223f0e135b2fc7f2c9539
Sylvain K <53495850+skamdem@users.noreply.github.com>
2021-07-01 12:14:21 +0000

    copy paste strings

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,9 +9,10 @@ let candidateAnswer = "";
 let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let candidateAnswers = [];
-let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
-let questions = ['Who was the first American woman in space? ', 'Ture or False: 5 kilometers = 5000 meters. ', '(5+3)/2*10 = ', 'Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2? ', 'What is the minimum crew size for the ISS? '];
-
+let questions = [];
+questions.push("Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? ");
+let correctAnswers = [];
+correctAnswers.push("Sally Ride", "true", "40", "Trajectory", "3");
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
  let candidateName = input.question("Enter your name: ");
